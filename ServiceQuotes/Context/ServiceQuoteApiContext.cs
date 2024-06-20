9d954854f67ea36c0af7a46342570ddb41274a82
Evandro Costa <evandro.souzac@gmail.com>
2024-06-20 17:12:08 -0300

    Adds EF Core Package and DB Context

---
+++ ServiceQuotes/Context/ServiceQuoteApiContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceQuotes.Models;
+
+namespace ServiceQuotes.Context;
+
+public class ServiceQuoteApiContext : DbContext
+{
+    public ServiceQuoteApiContext(DbContextOptions<ServiceQuoteApiContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Quote> Quotes { get; set; }
+    public DbSet<QuoteProducts> QuotesProducts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Quote>()
+            .HasMany(e => e.Products)
+            .WithMany(e => e.Quotes)
+            .UsingEntity<QuoteProducts>();
+    }
+}
