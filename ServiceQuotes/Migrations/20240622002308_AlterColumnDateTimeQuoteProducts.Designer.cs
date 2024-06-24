537853b3b5fd03196a0ca62b9a3a7530241a50b2
Evandro Costa <evandro.souzac@gmail.com>
2024-06-24 18:03:52 -0300

    Add Migrations to drop timestamp column

--- ServiceQuotes/Migrations/20240622002308_AlterColumnDateTimeQuoteProducts.Designer.cs
+++ ServiceQuotes/Migrations/20240622002308_AlterColumnDateTimeQuoteProducts.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ServiceQuotes.Context;
 
@@ -11,9 +12,11 @@ using ServiceQuotes.Context;
 namespace ServiceQuotes.Migrations
 {
     [DbContext(typeof(ServiceQuoteApiContext))]
-    partial class ServiceQuoteApiContextModelSnapshot : ModelSnapshot
+    [Migration("20240622002308_AlterColumnDateTimeQuoteProducts")]
+    partial class AlterColumnDateTimeQuoteProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
