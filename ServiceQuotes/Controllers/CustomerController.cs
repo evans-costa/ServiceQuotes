27143dd8d517ac477971f2281cbbd6d26725f5f1
Evandro Costa <evandro.souzac@gmail.com>
2024-06-24 18:08:43 -0300

    Adds POST and GET methods to controllers

---
+++ ServiceQuotes/Controllers/CustomerController.cs
@@ -0,0 +1,87 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using ServiceQuotes.DTOs;
+using ServiceQuotes.Models;
+using ServiceQuotes.Pagination;
+using ServiceQuotes.Repositories.Interfaces;
+using X.PagedList;
+
+namespace ServiceQuotes.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CustomerController : ControllerBase
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public CustomerController(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    private ActionResult<IEnumerable<CustomerRequestDTO>> GetCustomers(IPagedList<Customer> customers)
+    {
+        var metadata = new
+        {
+            customers.Count,
+            customers.PageSize,
+            customers.PageCount,
+            customers.TotalItemCount,
+            customers.HasNextPage,
+            customers.HasPreviousPage,
+        };
+
+        Response.Headers.Append("X-Pagination", JsonConvert.SerializeObject(metadata));
+
+        var customerDto = _mapper.Map<IEnumerable<CustomerRequestDTO>>(customers);
+
+        return Ok(customerDto);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CustomerRequestDTO>>> GetAll([FromQuery] QueryParameters customerParams)
+    {
+        var customers = await _unitOfWork.CustomerRepository.GetCustomersAsync(customerParams);
+
+        if (customers is null || customers.IsNullOrEmpty())
+        {
+            return NotFound();
+        }
+
+        return GetCustomers(customers);
+    }
+
+    [HttpGet("{id:guid}", Name = "GetCustomer")]
+    public async Task<ActionResult<CustomerRequestDTO>> GetById(Guid id)
+    {
+        var customer = await _unitOfWork.CustomerRepository.GetAsync(c => c.CustomerId == id);
+
+        if (customer is null)
+            return NotFound();
+
+        var customerDto = _mapper.Map<CustomerRequestDTO>(customer);
+
+        return Ok(customerDto);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<CustomerRequestDTO>> Create(CustomerRequestDTO customerDto)
+    {
+        if (customerDto == null)
+            return BadRequest();
+
+        var customer = _mapper.Map<Customer>(customerDto);
+
+        var createdCustomer = _unitOfWork.CustomerRepository.Create(customer);
+
+        await _unitOfWork.CommitAsync();
+
+        var newCustomer = _mapper.Map<CustomerRequestDTO>(createdCustomer);
+
+        return new CreatedAtRouteResult("GetCustomer", new { id = newCustomer.CustomerId }, newCustomer);
+    }
+}
---
+++ ServiceQuotes/Controllers/ProductController.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using ServiceQuotes.DTOs;
+using ServiceQuotes.Models;
+using ServiceQuotes.Pagination;
+using ServiceQuotes.Repositories.Interfaces;
+using X.PagedList;
+
+namespace ServiceQuotes.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public ProductController(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _mapper = mapper;
+        _unitOfWork = unitOfWork;
+    }
+
+    private ActionResult<IEnumerable<ProductRequestDTO>> GetProducts(IPagedList<Product> product)
+    {
+        var metadata = new
+        {
+            product.Count,
+            product.PageNumber,
+            product.PageSize,
+            product.TotalItemCount,
+            product.HasNextPage,
+            product.HasPreviousPage,
+        };
+
+        Response.Headers.Append("X-Pagination", JsonConvert.SerializeObject(metadata));
+
+        var productDto = _mapper.Map<IEnumerable<ProductRequestDTO>>(product);
+
+        return Ok(productDto);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ProductRequestDTO>>> GetAll([FromQuery] QueryParameters productParams)
+    {
+        var products = await _unitOfWork.ProductRepository.GetProductsAsync(productParams);
+
+        if (products is null || products.IsNullOrEmpty())
+        {
+            return NotFound();
+        }
+
+        return GetProducts(products);
+    }
+
+    [HttpGet("{id:guid}", Name = "GetProduct")]
+    public async Task<ActionResult<ProductRequestDTO>> GetById(Guid
+        id)
+    {
+        var product = await _unitOfWork.ProductRepository.GetAsync(p => p.ProductId == id);
+
+        if (product is null)
+            return NotFound();
+
+        var productDto = _mapper.Map<ProductRequestDTO>(product);
+
+        return Ok(productDto);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ProductRequestDTO>> Create(ProductRequestDTO productDto)
+    {
+        if (productDto is null)
+        {
+            return BadRequest();
+        }
+
+        var product = _mapper.Map<Product>(productDto);
+
+        var createdProduct = _unitOfWork.ProductRepository.Create(product);
+
+        await _unitOfWork.CommitAsync();
+
+        var newProduct = _mapper.Map<ProductRequestDTO>(createdProduct);
+
+        return new CreatedAtRouteResult("GetProduct", new
+        {
+            id = newProduct.ProductId
+        }, newProduct);
+    }
+}
