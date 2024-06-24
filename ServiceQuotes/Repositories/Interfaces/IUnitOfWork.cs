f3fe5c5b85f579013d2f45466e4ce2f4e761f026
Evandro Costa <evandro.souzac@gmail.com>
2024-06-24 18:04:44 -0300

    Adds `Unit of Work` repository

---
+++ ServiceQuotes/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace ServiceQuotes.Repositories.Interfaces;
+
+public interface IUnitOfWork
+{
+    ICustomerRepository CustomerRepository { get; }
+    IProductRepository ProductRepository { get; }
+    Task CommitAsync();
+    Task DisposeAsync();
+}
---
+++ ServiceQuotes/Repositories/UnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using ServiceQuotes.Context;
+using ServiceQuotes.Repositories.Interfaces;
+
+namespace ServiceQuotes.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private ICustomerRepository? _customerRepository;
+    private IProductRepository? _productRepository;
+    public ServiceQuoteApiContext _context;
+
+    public UnitOfWork(ServiceQuoteApiContext context)
+    {
+        _context = context;
+    }
+
+    public ICustomerRepository CustomerRepository
+    {
+        get
+        {
+            return _customerRepository ??= new CustomerRepository(_context);
+        }
+    }
+
+    public IProductRepository ProductRepository
+    {
+        get
+        {
+            return _productRepository ??= new ProductRepository(_context);
+        }
+    }
+
+    public async Task CommitAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _context.DisposeAsync();
+    }
+}
