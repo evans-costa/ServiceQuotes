311dbb757e3a3fa25c5878ebcf8e5ea64f810720
Evandro Costa <evandro.souzac@gmail.com>
2024-07-01 14:08:38 -0300

    Add Reference handler for JSON to ignore cycles on serialization

--- ServiceQuotes/Program.cs
+++ ServiceQuotes/Program.cs
@@ -3,11 +3,16 @@ using ServiceQuotes.Context;
 using ServiceQuotes.DTOs.Mappings;
 using ServiceQuotes.Repositories;
 using ServiceQuotes.Repositories.Interfaces;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -22,9 +27,11 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IQuotesRepository, QuotesRepository>();
 
 builder.Services.AddAutoMapper(typeof(CustomerDTOMappingProfile));
 builder.Services.AddAutoMapper(typeof(ProductDTOMappingProfile));
+builder.Services.AddAutoMapper(typeof(QuoteDTOMappingProfile));
 
 var app = builder.Build();
 
