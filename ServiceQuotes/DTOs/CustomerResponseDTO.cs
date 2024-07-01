128fa830e8d63f9e8a36f368ccb6ee0f17e8b22a
Evandro Costa <evandro.souzac@gmail.com>
2024-07-01 14:06:33 -0300

    Organize DTO in separate folders by request and response

--- ServiceQuotes/DTOs/CustomerResponseDTO.cs
+++ ServiceQuotes/DTOs/CustomerResponseDTO.cs
@@ -1,8 +1,7 @@
 ﻿namespace ServiceQuotes.DTOs;
 
-public class CustomerRequestDTO
+public class CustomerResponseDTO
 {
-    public Guid CustomerId { get; set; }
     public string? Name { get; set; }
     public string? Phone { get; set; }
 }
--- ServiceQuotes/DTOs/ProductRequestDTO.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ServiceQuotes.DTOs;
-
-public class ProductRequestDTO
-{
-    public Guid ProductId { get; set; }
-    public string? Name { get; set; }
-}
