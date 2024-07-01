cbd091ab644add544f4415f3f9f49ce65150ebea
Evandro Costa <evandro.souzac@gmail.com>
2024-07-01 14:05:46 -0300

    Creates Quote Mapping

---
+++ ServiceQuotes/DTOs/Mappings/QuoteDTOMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ServiceQuotes.DTOs.RequestDTO;
+using ServiceQuotes.DTOs.ResponseDTO;
+using ServiceQuotes.Models;
+
+namespace ServiceQuotes.DTOs.Mappings;
+
+public class QuoteDTOMappingProfile : Profile
+{
+    public QuoteDTOMappingProfile()
+    {
+        CreateMap<Quote, QuoteRequestDTO>().ReverseMap();
+        CreateMap<Quote, QuoteResponseDTO>().ReverseMap();
+
+        CreateMap<Quote, QuoteDetailedResponseDTO>()
+            .ForMember(dest => dest.Products, opt => opt.MapFrom(src => src.Products))
+            .ReverseMap();
+
+        CreateMap<QuoteProducts, QuoteProductsRequestDTO>().ReverseMap();
+    }
+}
