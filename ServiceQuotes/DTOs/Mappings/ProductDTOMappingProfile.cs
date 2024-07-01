067c9f0d6134c34ccadcccc286a1be6e92fa31a1
Evandro Costa <evandro.souzac@gmail.com>
2024-07-01 14:06:09 -0300

    Mapping members separatally for product DTO

--- ServiceQuotes/DTOs/Mappings/ProductDTOMappingProfile.cs
+++ ServiceQuotes/DTOs/Mappings/ProductDTOMappingProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using ServiceQuotes.DTOs.ResponseDTO;
 using ServiceQuotes.Models;
 
 namespace ServiceQuotes.DTOs.Mappings;
@@ -7,6 +8,11 @@ public class ProductDTOMappingProfile : Profile
 {
     public ProductDTOMappingProfile()
     {
-        CreateMap<Product, ProductRequestDTO>().ReverseMap();
+        CreateMap<Product, ProductDTO>().ReverseMap();
+        CreateMap<Product, ProductDetailDTO>()
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.QuoteProducts.FirstOrDefault().Price))
+            .ForMember(dest => dest.Quantity, opt => opt.MapFrom(src => src.QuoteProducts.FirstOrDefault().Quantity))
+            .ReverseMap();
     }
 }
