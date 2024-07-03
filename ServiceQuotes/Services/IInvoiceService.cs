4e28fae34c9c6e18ffacccced5f9204a8c5cf269
Evandro Costa <evandro.souzac@gmail.com>
2024-07-03 18:03:22 -0300

    Creates `InvoiceService` to generate pdf invoice files

---
+++ ServiceQuotes/Services/IInvoiceService.cs
@@ -0,0 +1,8 @@
+﻿using ServiceQuotes.DTOs.ResponseDTO;
+
+namespace ServiceQuotes.Services;
+
+public interface IInvoiceService
+{
+    byte[] GenerateInvoiceDocument(QuoteDetailedResponseDTO quote);
+}
---
+++ ServiceQuotes/Services/InvoiceService.cs
@@ -0,0 +1,34 @@
+﻿using QuestPDF.Fluent;
+using ServiceQuotes.DTOs.ResponseDTO;
+using ServiceQuotes.Helpers;
+using ServiceQuotes.Models;
+
+namespace ServiceQuotes.Services;
+
+public class InvoiceService : IInvoiceService
+{
+    public byte[] GenerateInvoiceDocument(QuoteDetailedResponseDTO quote)
+    {
+        var invoice = new Invoice
+        {
+            InvoiceNumber = quote.QuoteId,
+            CreatedAt = quote.CreatedAt,
+
+            CustomerInfo = new CustomerInfo
+            {
+                Name = quote.CustomerInfo?.Name,
+                Phone = quote.CustomerInfo?.Phone
+            },
+
+            Items = quote.Products?.Select(product => new OrderItem
+            {
+                Name = product.Name,
+                Price = product.Price,
+                Quantity = product.Quantity,
+            }).ToList()
+        };
+
+        var document = new InvoiceDocumentTemplate(invoice);
+        return document.GeneratePdf();
+    }
+}
