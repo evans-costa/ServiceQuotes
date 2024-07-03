d5e6f87f6cf0829235a7052182e64c5ee28eae23
Evandro Costa <evandro.souzac@gmail.com>
2024-07-03 18:04:10 -0300

    Install `questPDF` package and adds to DI Container

--- ServiceQuotes/Program.cs
+++ ServiceQuotes/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using QuestPDF.Infrastructure;
 using ServiceQuotes.Context;
 using ServiceQuotes.DTOs.Mappings;
 using ServiceQuotes.Repositories;
 using ServiceQuotes.Repositories.Interfaces;
+using ServiceQuotes.Services;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,11 +25,14 @@ builder.Services.AddDbContext<ServiceQuoteApiContext>(options =>
     options.UseSqlServer(sqlServerConnection);
 });
 
+QuestPDF.Settings.License = LicenseType.Community;
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IQuotesRepository, QuotesRepository>();
+builder.Services.AddScoped<IInvoiceService, InvoiceService>();
 
 builder.Services.AddAutoMapper(typeof(CustomerDTOMappingProfile));
 builder.Services.AddAutoMapper(typeof(ProductDTOMappingProfile));
