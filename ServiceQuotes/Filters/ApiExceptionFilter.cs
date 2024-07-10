4abfbd23407e672b0c3bce15063a031c0b3b0965
Evandro Costa <evandro.souzac@gmail.com>
2024-07-10 18:15:16 -0300

    Adds Exception filter to handle global exceptions

---
+++ ServiceQuotes/Filters/ApiExceptionFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ServiceQuotes.Filters;
+
+public class ApiExceptionFilter : IExceptionFilter
+{
+    private readonly ILogger<ApiExceptionFilter> _logger;
+
+    public ApiExceptionFilter(ILogger<ApiExceptionFilter> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnException(ExceptionContext context)
+    {
+        _logger.LogError(context.Exception, "Um erro desconhecido ocorreu.");
+
+        context.Result = new ObjectResult("Um erro desconhecido ocorreu.")
+        {
+            StatusCode = StatusCodes.Status500InternalServerError,
+        };
+    }
+}
