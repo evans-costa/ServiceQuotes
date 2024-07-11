676ce8a486d8c5f5f66a36410af14e646b8e948c
Evandro Costa <evandro.souzac@gmail.com>
2024-07-11 19:50:39 -0300

    Refactor Exception Middleware to show 5xx errors

--- ServiceQuotes/Extensions/ExceptionMiddlewareExtensions.cs
+++ ServiceQuotes/Extensions/ExceptionMiddlewareExtensions.cs
@@ -6,7 +6,7 @@ namespace ServiceQuotes.Extensions;
 
 public static class ExceptionMiddlewareExtensions
 {
-    public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+    public static void ConfigureExceptionHandler(this IApplicationBuilder app, IHostEnvironment environment)
     {
         app.UseExceptionHandler(appError =>
         {
@@ -19,12 +19,27 @@ public static class ExceptionMiddlewareExtensions
 
                 if (contextFeature != null)
                 {
-                    await context.Response.WriteAsync(new ErrorDetails()
+                    var logger = app.ApplicationServices.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(contextFeature.Error, "Unhandled exception occurred: {Trace}.", contextFeature.Error.StackTrace);
+
+
+                    if (environment.IsDevelopment())
                     {
-                        StatusCode = context.Response.StatusCode,
-                        Message = contextFeature.Error.Message,
-                        Trace = contextFeature.Error.StackTrace
-                    }.ToString());
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = contextFeature.Error.Message,
+                            Trace = contextFeature.Error.StackTrace
+                        }.ToString());
+                    }
+                    else
+                    {
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Um erro desconhecido ocorreu.",
+                        }.ToString());
+                    }
                 };
             });
         });
